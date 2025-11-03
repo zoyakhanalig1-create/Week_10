5683480e8a882314b9d0f487f8ab32d0cedaec20
Zoya khan <zoyakhanalig1@gmail.com>
2025-11-03 13:35:55 +0530

    Create Week10(1).cpp

---
+++ Week10(1).cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Sorting (Bubble Sort)
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Array in Ascending Order:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
